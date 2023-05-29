976f9212fd39cf4bf896d34d54af18991a53d845
Marianna <121226166+mari-albrici@users.noreply.github.com>
2023-05-29 14:01:03 +0200

    Project created

---
+++ src/main/java/mariannaalbrici/BED16/BeD16Application.java
@@ -0,0 +1,13 @@
+package mariannaalbrici.BED16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeD16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeD16Application.class, args);
+	}
+
+}
---
+++ src/test/java/mariannaalbrici/BED16/BeD16ApplicationTests.java
@@ -0,0 +1,13 @@
+package mariannaalbrici.BED16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeD16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
